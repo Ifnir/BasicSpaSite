42e32ea332fff1f7a491ffafb38fd7a97d495e71
Andreas <demikz.co@gmail.com>
2019-06-08 12:38:47 +0200

    added packages to project

--- components/server.js
+++ components/server.js
@@ -3,7 +3,9 @@ const path = require('path')
 const exphbs = require('express-handlebars')
 const app = express()
 const favicon = require('serve-favicon')
-var cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const routes = require('./routes/index')
 
 const PORT = process.env.PORT || 3000
@@ -12,6 +14,15 @@ const hbs = exphbs.create({ defaultLayout: 'main' })
 
 app.engine('handlebars', hbs.engine)
 app.set("view engine", "handlebars")
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}))
+
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
 
 // Routes
 app.use(routes)

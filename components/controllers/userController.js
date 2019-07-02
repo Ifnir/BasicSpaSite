d4ed7a185e97a0adc05d5ae7c7baebb9faebd454
Andreas <demikz.co@gmail.com>
2019-07-02 19:44:51 +0200

    small fix

--- components/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-const Controller = require('./Controller')
-const c = new Controller
-class UserController extends c {
-    foo(){
-        fetcha
-    }
-}
--- components/routes/auth.js
+++ components/routes/auth.js
@@ -1,6 +1,5 @@
 const app = module.exports = require('express')()
 
-const { login } = require('./../controllers/auth')
 
 app.post('/login', (req, res) => {
     
